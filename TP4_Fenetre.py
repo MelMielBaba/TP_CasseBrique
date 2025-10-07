99a8c9a21460903b99a9ba7d4f54864451c91b58
ML.M--B <ml.m--badel@orange.fr>
2025-10-07 08:51:44 +0200

    TPCasseBrique - Save (2) [071025] {08:52} /Mel\

---
+++ TP4_Fenetre.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Date de creation : 7 octobre 2025
+Auteur: Marie Louise MILLIEN & Elouen WURMSER
+Projet: TP4 - Casse-Brique
+Titre: Fichier de la Fenetre
+"""
+
+#Importation des modules
+import tkinter as tk
+
+def proit():
+    pass
---
+++ TP4_ReadMe.txt
@@ -0,0 +1,6 @@
+"""
+Date de creation : 7 octobre 2025
+Auteur: Marie Louise MILLIEN & Elouen WURMSER
+Projet: TP4 - Casse-Brique
+Titre: Fichier README
+"""
