ff171be7d509b4436871333ee8d072ae3bdf4d9e
ML.M--B <ml.m--badel@orange.fr>
2025-10-07 10:32:02 +0200

    TP_CB_Mel - Save (1) [071025] {10:32} /Mel\

--- TP4_Fenetre.py
+++ TP4_Fenetre.py
@@ -9,3 +9,41 @@ Titre: Fichier de la Fenetre
 #Importation des modules
 import tkinter as tk
 
+#Creation de la classe Fenetre
+class Fenetre:
+    """
+    La classe fenetre est parente de toutes les fenetres du jeu. 
+    Elle a donc en attribut une fenetre et un bouton quitter
+    """
+    def __init__(self):
+        pass
+
+#Creation de la classe de la fenetre principale
+class Fenetre_principale(Fenetre):
+    """
+    Cette classe 'creer' la fenetre qui se lance au debut du jeu.
+    Elle possede comme toute fenetre un bouton quitter (heriter de Fenetre()),
+    un bouton option et un bouton jouer
+    """
+    def __init__(self):
+        pass
+
+#Creation de la classe de la fenetre de jeu
+class Fenetre_jeu(Fenetre):
+    """
+    Cette classe 'creer' la fenetre sur laquelle se deroule le jeu.
+    Elle possede comme toute fenetre un bouton quitter (heriter de Fenetre()),
+    un bouton menu un caneva
+    """
+    def __init__(self):
+        pass
+
+#Creation de la classe de la fenetre de jeu
+class Fenetre_Option(Fenetre):
+    """
+    Cette classe 'creer' la fenetre du menu des options.
+    Elle possede comme toute fenetre un bouton quitter (heriter de Fenetre()),
+    un bouton retour
+    """
+    def __init__(self):
+        pass
