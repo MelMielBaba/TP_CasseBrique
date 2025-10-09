b48be1a8d5fe1f326dd7eaade2fa0a93542546e9
ML.M--B <ml.m--badel@orange.fr>
2025-10-09 19:15:30 +0200

    TP-CB ~Mel-Branche2~ Save (1) [091025] {19:16} /Mel\
    -> Reprise de TP4_Fenetre
    -> Essaie d'appel depuis TP4_Jeu.py
    
    =>Tentatives sans succes

--- TP4_Fenetre.py
+++ TP4_Fenetre.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+#En-tete
 """
 Date de creation : 7 octobre 2025
 Auteurs: Marie Louise MILLIEN & Elouen WURMSER
@@ -6,17 +8,16 @@ Projet: TP4 - CasseBrique
 Titre: Fichier des Fenetres
 """
 
+#Description
 """
 Description du fichier:
     Ce fichier implémente la classe principale App qui gère la navigation entre les 3 écrans:
-    - FenetreDemarrage : écran de démarrage avec les boutons Jouer, Option, Quitter
-    - FenetreOption : écran des options
-    - FenetreJeu : écran contenant le canvas de jeu et le score
-
-    Les transitions entre les fenetres sont réalisées en détruisant la frenetre courante
-    puis en créant la suivante.
+    - Fenetre_principale : écran de démarrage avec les boutons Jouer, Option, Quitter
+    - Fenetre_option : écran des options
+    - Fenetre_jeu : écran contenant le canvas de jeu et le score
 """
 
+#TO DO
 """
 TO DO :
     - Redéfinir les methodes de Fenetre(), fonctionnement et description en commentaire
@@ -38,7 +39,7 @@ TO DO :
 #Importation des modules
 import tkinter as tk
 
-class Fenetre:
+class Fenetre(tk.Tk):
     """
     Description : Classe Mere des fenetres du jeu
     Attributs :
@@ -46,143 +47,163 @@ class Fenetre:
     Methodes :
         -
     """
-    def __init__(self):
+    def __init__(self,nom_fenetre):
+        super().__init__()
         #creation de la fenetre graphique
-        self.fenetre_racine = tk.Tk()
+        self.f_fenetre_racine = tk.Tk()
         #taille de la fenetre
-        self.fenetre_racine.geometry("400x400")
+        self.f_fenetre_racine.geometry("400x400")
+        #nom de la fenetre
+        self.__f_nom_fenetre = nom_fenetre 
 
-        self.creation_fenetre_demarrage()
- 
-    def creation_fenetre_demarrage(self):
-        """
-        Fonction : Crée et affiche l'écran de démarrage. La méthode instancie 
-        FenetreDemarrage, lie les boutons aux méthodes de app pour naviguer 
-        vers les autres écrans, puis affiche la fenetre avec pack().
-        Entree : self
-        Sortie : 
-        """
-        self.start_frame = Fenetre_principale(self.root, self)
-        self.start_frame.btn_option.bind('<Button-1>', self.creation_fenetre_option)
-        self.start_frame.btn_jouer.bind('<Button-1>', self.creation_fenetre_jeu)
-        self.start_frame.pack()
- 
-    def creation_fenetre_option(self, event=None):
-        """
-        Fonction : Transition vers la fenetre des options. Paramètre event : 
-        reçu quand la méthode est appelée via bind. Permet à la meme 
-        fonction d'être utilisée comme retour d'appel.
-        Entree : self
-        Sortie : 
-        """
-        self.start_frame.destroy()
-        self.second_menu_frame = Fenetre_option(self.root, self)
-        self.second_menu_frame.pack()
+        # --- Constantes ---
+        self.f_fenetre_width = 1000
+        self.f_fenetre_height = 650
+        self.f_canevas_width = 900
+        self.f_canevas_height = 500
+        self.f_titre = "Casse Brique"
     
-    def creation_fenetre_jeu(self, event=None):
+    def fermer_fenetre(self):
         """
-        Fonction : Transition vers la fenetre de jeu. Paramètre event : 
-        reçu quand la méthode est appelée via bind. Permet à la meme 
-        fonction d'être utilisée comme retour d'appel.
+        Fonction : Fermer la fenetre tkinter
         Entree : self
-        Sortie : 
+        Sortie : None
         """
-        self.start_frame.destroy()       
-        self.third_menu_frame = Fenetre_jeu(self.root, self)
-        self.third_menu_frame.pack()
+        self.f_fenetre_racine.destroy
     
-    def retour_demarrage_option(self):
-        """
-        Fonction : Retour au menu de démarrage depuis la fenetre d'options.
-        C'est la fonction d'appel du bouton retour de la fenetre d'options
-        Entree : self
-        Sortie : 
-        """
-        self.second_menu_frame.destroy()
-        self.creation_fenetre_demarrage()
+    def get_nom_fenetre(self):
+        return self.__f_nom_fenetre
 
-    def retour_demarrage_jeu(self):
+
+class Manager_fenetres(Fenetre):
+    """
+    Description : Gere les differentes fenetres
+    Attributs :
+        - mf_stock_fenetre : le dictionnaire qui stock les fenetres
+        - mf_fenetre_courante : la fenetre actuellement affichee
+        - 
+    Methodes :
+        - ajouter_nouvelle_fenetre()
+        - afficher_fenetre_actuelle()
+        -
+    """
+    def __init__(self):
+        #Stockage des fentetres dans un dictionnaire
+        self.mf_stock_fenetre = dict() 
+        self.mf_fenetre_courante = "Fenetre principale"
+
+    def ajouter_nouvelle_fenetre(self,anf_nom_fenetre:str):
         """
-        Fonction : Retour au menu de démarrage depuis la fenetre de jeu.
-        C'est la fonction d'appel du bouton retour de la fenetre de jeu
-        Entree : self
-        Sortie : 
+        Fonction : Permet d'ajouter une nouvelle fenetre grace a son nom dans 
+        le dictionnaire qui stock toutes les fenetres
+        Entree : self, le nom de la fenetre (STR)
+        Sortie : None
         """
-        self.third_menu_frame.destroy()
-        self.creation_fenetre_demarrage()
- 
- 
-class Fenetre_principale(tk.Frame):
+        if anf_nom_fenetre == "Fenetre principale":
+            anf_fenetre_ajoute = Fenetre_principale()
+            self.mf_stock_fenetre[anf_nom_fenetre] = anf_fenetre_ajoute
+        elif anf_nom_fenetre == "Fenetre options":
+            anf_fenetre_ajoute = Fenetre_option()
+            self.mf_stock_fenetre[anf_nom_fenetre] = anf_fenetre_ajoute
+        elif anf_nom_fenetre == "Fenetre jeu" :
+            anf_fenetre_ajoute = Fenetre_jeu()
+            self.mf_stock_fenetre[anf_nom_fenetre] = anf_fenetre_ajoute
+
+    def afficher_fenetre_actuelle(self,afa_nom_fenetre:str):
+        """
+        Fonction : Gere le changement de la fenetre actuellement affichee
+        Entree : self, le nom de la fenetre (STR)
+        Sortie : None
+        """
+        afa_fenetre_affichee = self.mf_stock_fenetre[afa_nom_fenetre]
+        afa_fenetre_affichee.tk.tkraise()
+    
+    """
+    def lancer_fenetre_courante(self):
+        self.mf_stock_fenetre[self.mf_fenetre_courante].mainloop()
+    """
+
+    def get_fenetre(self,gf_nom_fenetre):
+        return self.mf_stock_fenetre[gf_nom_fenetre]
+
+
+class Fenetre_principale(Fenetre):
     """
     Description : Classe Fille Fenetre de demarrage, affichee en 
     premier lorsqu'on lance le jeu via TP4_Jeu.py
     Attributs :
-        - super().__init__(self) : recuperation des attributs parents, cad de Fenetre()
+        - super().__init__() : recuperation des attributs parents, cad de Fenetre()
         - fp_btn_jouer, fp_btn_option, fp_btn_quitter : implementation des boutons Jouer,
         Option et Quitter
     Methodes :
         -
     """
-    def __init__(self, parent, app):
-        super().__init__(parent)
-        self.app = app
+    def __init__(self):
+        super().__init__("Fenetre principale")
         
+        """
         #Affichage du titre de la fenetre
-        tk.Label(self, 
+        f_fenetre_racine.Label(self, 
                  text='Casse Brique', 
                  font=("Arial", 20, "bold")).pack(pady=20)
+        """
 
         #►LES BOUTONS◄
         #Creation du bouton pour lancer la fenetre de jeu
-        self.fp_btn_jouer = tk.Button(self, text="Jouer")
-        self.fp_btn_jouer.pack(pady=10)
+        self.fp_btn_jouer = tk.Button(self, text = "Jouer")
+        self.fp_btn_jouer.pack(pady = 10)
 
         #Creation du bouton pour lancer la fenetre des options
-        self.fp_btn_option = tk.Button(self, text="Option")
-        self.fp_btn_option.pack(pady=10)
+        self.fp_btn_option = tk.Button(self, text = "Option")
+        self.fp_btn_option.pack(pady = 10)
 
         #Creation du bouton pour quitter definitivement la fenetre
         self.fp_btn_quitter = tk.Button(self, 
-                                     text="Quitter", 
-                                     command=app.root.destroy) # arrete le programme en cours
-        self.fp_btn_quitter.pack(pady=10)
+                                     text = "Quitter", 
+                                     command = self.fermer_fenetre())
+        self.fp_btn_quitter.pack(pady = 10)
  
  
-class Fenetre_option(tk.Frame):
+class Fenetre_option(Fenetre):
     """
     Description : Classe Fille Fenetre du menu des options
     Attributs :
-        - super().__init__(self) : recuperation des attributs parents, cad de Fenetre()
+        - super().__init__() : recuperation des attributs parents, cad de Fenetre()
         - fo_btn_option1, fo_btn_retour, fo_btn_quitter : implementation des 
         boutons Option1 (a definir), Retour qui renvoie a la fenetre principal et Quitter
     Methodes :
         - Aucunes
     """
-    def __init__(self, parent, app):
-        super().__init__(parent)
-        self.app = app
+    def __init__(self):
+        super().__init__("Fenetre options")
 
         #Affichage du titre de la fenetre
         tk.Label(self, 
-                 text='Options', 
-                 font=("Arial", 20, "bold")).pack(pady=20)
+                 text = 'Options', 
+                 font = ("Arial", 20, "bold")).pack(pady=20)
         
         #►LES BOUTONS◄
         #Creation des boutons des options
         #►OPTION 1
-        self.fo_btn_option1 = tk.Button(self, text="WIP")
-        self.fo_btn_option1.pack(pady=10)
+        self.fo_btn_option1 = tk.Button(self, text = "WIP1")
+        self.fo_btn_option1.pack(pady = 10)
+        #►OPTION 2
+        self.fo_btn_option2 = tk.Button(self, text="WIP2")
+        self.fo_btn_option2.pack(pady = 10)
 
         #Creation du bouton pour revenir a la fenetre principale
-        self.fo_btn_retour = tk.Button(self, text="Retour", 
-                                    command=app.retour_demarrage_option) #Voir fonction dans app
-        self.fo_btn_retour.pack(pady=10)
+        self.fo_btn_retour = tk.Button(self, text = "Retour", 
+                                    command = self.afficher_fenetre_actuelle("Fenetre principale")) 
+        self.fo_btn_retour.pack(pady = 10)
 
         #Creation du bouton pour quitter definitivement la fenetre
-        self.fo_btn_quitter = None
+        self.fo_btn_quitter = tk.Button(self, 
+                                     text = "Quitter", 
+                                     command = self.fermer_fenetre())
+        self.fo_btn_quitter.pack(pady = 10)
  
 
-class Fenetre_jeu(tk.Frame):
+class Fenetre_jeu(Fenetre):
     """
     Description : Classe Fille Fenetre du jeu, elle contient le canevas ou 
     se deroule le jeu casse brique
@@ -196,60 +217,55 @@ class Fenetre_jeu(tk.Frame):
     Methodes :
         - Aucunes
     """
-    def __init__(self, parent, app):
-        super().__init__(parent)
-        self.app = app
+    def __init__(self):
+        super().__init__(self)
         
         #►LES BARRES DE MENU ET D'INFOS◄
         #Creation de la barre de menu du haut
         self.fj_haut_barre = tk.Frame(self)
-        self.fj_haut_barre.pack(fill="x", pady=5, padx=5)
+        self.fj_haut_barre.pack(fill = "x", pady = 5, padx = 5)
 
         #Creation de la barre de menu du bas
         self.fj_bas_barre = tk.Frame(self)
-        self.fj_bas_barre.pack(side="bottom", fill="both", expand=True)
+        self.fj_bas_barre.pack(side = "bottom", fill = "both", expand = True)
 
         #►LES BOUTONS◄
         #Creation du bouton pour quitter definitivement la fenetre
         self.fj_btn_quitter = tk.Button(self.fj_bas_barre, #Emplacement du bouton (la barre du bas)
-                                     text="Quitter", 
-                                     command=app.root.destroy) 
-        self.fj_btn_quitter.pack(side="left", padx=5, pady=5)
+                                     text = "Quitter", 
+                                     command = self.fermer_fenetre()) 
+        self.fj_btn_quitter.pack(side = "left", padx = 5, pady = 5)
 
         #Creation du bouton pour revenir a la fenetre principale
         self.fj_btn_retour = tk.Button(self.fj_bas_barre, #Emplacement du bouton (la barre du bas)
-                                    text="Retour", 
-                                    command=app.retour_demarrage_jeu) 
-        self.fj_btn_retour.pack(side="left", 
-                             padx=5, 
-                             pady=5)
+                                    text = "Retour", 
+                                    command = self.afficher_fenetre_actuelle("Fenetre principale")) 
+        self.fj_btn_retour.pack(side = "left", 
+                             padx = 5, 
+                             pady = 5)
+
+        #Creation du bouton pour lancer le jeu
+        self.fj_btn_lancer = tk.Button(self.fj_bas_barre, #Emplacement du bouton (la barre du bas)
+                                    text = "Lancer le jeu", 
+                                    command = self.lancer_jeu()) 
+        self.fj_btn_lancer.pack(side = "left", 
+                             padx = 5, 
+                             pady = 5)
 
         #Creation du canevas ou se deroule le jeu
         self.fj_canvas = tk.Canvas(self, 
-                                width=1000, #Largeur
-                                height=500, #Hauteur
-                                bg="black") #Couleur
+                                f_canevas_width = 1000, #Largeur
+                                f_canevas_height = 500, #Hauteur
+                                bg = "black") #Couleur
         self.fj_canvas.pack()
 
         #Affichage du score
         self.fj_score = 0
-        self.fj_score_var = tk.StringVar(value=f"Score : {self.score}")
+        self.fj_score_var = tk.StringVar(value = f"Score : {self.score}")
         self.fj_score_label = tk.Label(self.top_bar,  # SCORE est dans la bare du haut
-                                    textvariable=self.score_var, # Affichage du texte variable
-                                    font=("Arial", 12, "bold"))  # Personalisation
+                                    textvariable = self.score_var, # Affichage du texte variable
+                                    font = ("Arial", 12, "bold"))  # Personalisation
         
-        self.fj_score_label.pack(side="left", 
-                              padx=10)
+        self.fj_score_label.pack(side = "left", 
+                              padx = 10)
 
-
-class Manager_fenetres:
-    """
-    Description : Gere les differentes fenetres
-    Attributs :
-        - 
-    Methodes :
-        - changement_fenetre()
-        - 
-    """
-    def __init__(self):
-        pass
--- TP4_Jeu.py
+++ TP4_Jeu.py
@@ -12,11 +12,23 @@ import TP4_Balle as B
 import TP4_Raquette as R
 
 #Creation des objets
-J_Balle = B.Balle()
-J_Raquette = R.Raquette()
+J_Balle = B.Balle(3,3,3)
+J_Raquette = R.Raquette(2,5)
+
+#Creation du gestionnaire des fenetres
+J_Manager_fenetres = F.Manager_fenetres()
 
 #Creation des objets fenetre
-J_Fenetre_principale = F.Fenetre_principale()
-J_Fenetre_jeu = F.Fenetre_jeu()
-J_Fenetre_option = F.Fenetre_option()
+#J_Fenetre_principale = F.Fenetre_principale()
+#J_Fenetre_jeu = F.Fenetre_jeu()
+#J_Fenetre_option = F.Fenetre_option()
 
+J_Manager_fenetres.ajouter_nouvelle_fenetre("Fenetre principale")
+#J_Manager_fenetres.ajouter_nouvelle_fenetre(J_Fenetre_option)
+#J_Manager_fenetres.ajouter_nouvelle_fenetre(J_Fenetre_jeu)
+
+#J_Manager_fenetres.lancer_fenetre_courante()
+
+print(J_Manager_fenetres.mf_stock_fenetre)
+print("baguette")
+print(J_Manager_fenetres.get_fenetre("Fenetre principale"))
