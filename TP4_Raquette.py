694cb603815c384fb8e5e0079aa5c4371ef53c21
ML.M--B <ml.m--badel@orange.fr>
2025-10-14 08:38:06 +0200

    TPCB ~MelBr3~ Save(6) [141025] {08:38} /Mel\

--- TP4_Raquette.py
+++ TP4_Raquette.py
@@ -9,15 +9,22 @@ Titre: Fichier de la classe Raquette
 class Raquette():
     def __init__(self,r_manager):
         self.__long = 5 #Longueur initiale de la barre
-        self.__haut = 2 #Heuteur FIXE de la barre
+        self.__haut = 2 #Hauteur FIXE de la barre
         #position initiale de la balle
         self.__xbarre = r_manager.get_largeur_canvas()/2
-        self.__ybarre = r_manager.get_hauteur_canvas/2
+        self.__ybarre = r_manager.get_hauteur_canvas()/2
     
+    def set_ybarre(self,syb_valeur:int):
+        self.__ybarre = syb_valeur
+
     def deplacement_barre(self,event):
         db_touche = event.keysym
         print(db_touche)
 
         #deplacement a gauche
         if db_touche == 'q':
+            pass
+
+        #deplacement a droite
+        elif db_touche == 'd':
             pass
