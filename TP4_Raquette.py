e8d132a3fecd90e0f7f0f0a15f02cb7ec0516e81
ML.M--B <ml.m--badel@orange.fr>
2025-10-14 09:42:04 +0200

    TPCB ~MelBr3~ Save(7) [141025] {09:42} /Mel\
    -> Importation du fichier TP4_Constantes.py (creer par Elouen) dans les fichiers TP4_Balle et TP4_Raquette
    -> Remise au propre des noms de variables

--- TP4_Raquette.py
+++ TP4_Raquette.py
@@ -6,25 +6,45 @@ Projet: TP4 - CasseBrique
 Titre: Fichier de la classe Raquette
 """
 
+#Importation du fichier des constantes
+import TP4_Constantes as C
+
 class Raquette():
     def __init__(self,r_manager):
-        self.__long = 5 #Longueur initiale de la barre
-        self.__haut = 2 #Hauteur FIXE de la barre
+        self.__r_long = C.c_longeur_raquette
+        self.__r_haut = C.c_hauteur_raquette
+        #recuperation des infos du canevas
+        self.r_lc = C.c_largeur_canvas
+        self.r_hc = C.c_hauteur_canvas
         #position initiale de la balle
-        self.__xbarre = r_manager.get_largeur_canvas()/2
-        self.__ybarre = r_manager.get_hauteur_canvas()/2
-    
-    def set_ybarre(self,syb_valeur:int):
-        self.__ybarre = syb_valeur
+        self.__r_xbarre = self.r_lc/2
+        self.__r_ybarre = self.r_hc/2
+
+        self.__r_vitesse = C.c_vitesse_raquette
+
+        
+    def change_xbarre(self,cxb_valeur:int):
+        """
+        Fonction : modifie la valeur de la position en y en ajoutant 
+        une valeur (exemple pour 5 y+5 ou y-5)
+        Entree : la valeur a ajouter (INT)
+        Sortie : None
+        """
+        self.__r_xbarre += cxb_valeur
 
     def deplacement_barre(self,event):
+        """
+        Fonction : Gere le deplacement de la barre en fonction des touches du clavier
+        Entree : un evenement de tkinter
+        Sortie : None
+        """
         db_touche = event.keysym
         print(db_touche)
 
         #deplacement a gauche
-        if db_touche == 'q':
-            pass
+        if db_touche == 'q' or db_touche == 'Left':
+            self.change_xbarre(+self.__r_vitesse)
 
         #deplacement a droite
-        elif db_touche == 'd':
-            pass
+        elif db_touche == 'd' or db_touche == 'Right':
+            self.change_xbarre(-self.__r_vitesse)
